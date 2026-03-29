5ad4611f6fdd5be460dd72188ea7b90a1c60f109
Naman <namansarvaiya0.com>
2026-03-29 13:47:30 +0530

    feat: added support for direct MySQL public URL connection

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
+const db = mysql.createPool(process.env.MYSQL_PUBLIC_URL || {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
